d13c513053b49b3ef6484bf00542d6da0216fc81
William <william.l.munoz@gmail.com>
2020-04-22 18:46:40 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,16 +1,16 @@
 class Person
 def name = (persons_name)
-  @name = persons_name
+  @this_persons_name = persons_name
 end
 
 def name
-  @name
+  @this_persons_name
 end
 
-def job = (job)
-  @job = job
+def job = (persons_job)
+  @this_persons_job = persons_job
   end
 
   def job
-    @job
+    @this_persons_job
 end
